30bca5ae36433d812a5339265d648828bf57bccc
黑天的禹 <1165736389@qq.com>
2025-01-17 13:22:36 +0800

    增加了一个专门对数据库操作的类

---
+++ flaskApp/db_operations.py
@@ -0,0 +1,44 @@
+# flaskApp/db_operations.py
+from flaskApp.models import SteelPrice, db
+from datetime import datetime
+from flaskApp.models import SteelPrice, db
+from datetime import datetime
+
+        def save_steel_price(name, price, timestamp):
+            existing_data = {f"{item.name}-{item.price}": item for item in SteelPrice.query.all()}
+            key = f"{name}-{price}"
+            if key not in existing_data:
+                steel_price = SteelPrice(name=name, price=price, timestamp=timestamp)
+                db.session.add(steel_price)
+                db.session.commit()
+
+        def get_all_steel_prices():
+            """
+            获取所有钢铁价格数据
+            """
+            return SteelPrice.query.all()
+
+        def get_steel_price_by_name(name):
+            """
+            根据名称获取钢铁价格数据
+            """
+            return SteelPrice.query.filter_by(name=name).first()
+
+        def update_steel_price(name, new_price):
+            """
+            更新指定名称的钢铁价格
+            """
+            steel_price = SteelPrice.query.filter_by(name=name).first()
+            if steel_price:
+                steel_price.price = new_price
+                steel_price.timestamp = datetime.now()
+                db.session.commit()
+
+        def delete_steel_price(name):
+            """
+            删除指定名称的钢铁价格数据
+            """
+            steel_price = SteelPrice.query.filter_by(name=name).first()
+            if steel_price:
+                db.session.delete(steel_price)
+                db.session.commit()
--- flaskApp/routes.py
+++ flaskApp/routes.py
@@ -14,34 +14,3 @@ def index():
     steel_names = [steel.name for steel in data]
     steel_prices = [steel.price for steel in data]
     return render_template('index.html', steel_names=steel_names, steel_prices=steel_prices)
-# @app.route('/')
-# def index():
-#     data = SteelPrice.query.all()
-#     data_list = [{'date': item.date, 'price': item.price, 'region': item.region} for item in data]
-#     return render_template('index.html', data=data_list)
-#
-#
-# @app.route('/api/data')
-# def api_data():
-#     data = SteelPrice.query.all()
-#     data_list = [{'date': item.date, 'price': item.price, 'region': item.region} for item in data]
-#     return jsonify(data_list)
-#
-#
-# @app.route('/api/data/<region>')
-# def api_data_by_region(region):
-#     data = SteelPrice.query.filter_by(region=region).all()
-#     data_list = [{'date': item.date, 'price': item.price} for item in data]
-#     return jsonify(data_list)
-#
-#
-# @app.route('/api/statistics')
-# def api_statistics():
-#     from data_processing import process_data
-#     df = process_data()
-#     average_price = df['price'].mean()
-#     average_price_by_region = df.groupby('region')['price'].mean().to_dict()
-#     return jsonify({
-#         'average_price': average_price,
-#         'average_price_by_region': average_price_by_region
-#     })
